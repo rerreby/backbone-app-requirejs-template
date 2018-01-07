3e53a2f08fcc76d875410fa570a74836e21cd7a8
rerreby <rerreby@rambler.ru>
2018-01-07 17:19:46 +0300

    Working prototype

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(init);
+
+function init() {
+    var AppView = Backbone.View.extend({
+        el: '#container',
+
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+            this.$el.html('Hello, World!');
+        }
+    });
+    
+    var appView = new AppView();
+}
